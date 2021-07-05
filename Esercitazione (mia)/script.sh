1ae5ee0fd1b3eb229fdea806331b90c2a4de56fe
emanuelecavalleri <33032169+emanuelecavalleri@users.noreply.github.com>
2021-07-05 18:09:45 +0200

    Delete Esercitazione (mia) directory

--- Esercitazione (mia)/script.sh
+++
@@ -1,7 +0,0 @@
-cd /Users/emanuelecavalleri/Desktop/Progetto
-fsharpc -a IntSort.fsi IntSorti.fs | echo 'Please wait...'
-echo -ne 'List length ?= '
-fsharpi intSort.fsx > out.txt
-cat out.txt
-echo
-echo '*** Output stored as "out.txt" ***'
